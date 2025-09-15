d83d397cd155b932b461d664acc3ca4be8743565
WearTheClo <67886325+WearTheClo@users.noreply.github.com>
2025-09-15 14:48:46 +0800

    Update fused_run.py

--- fused_run.py
+++ fused_run.py
@@ -14,12 +14,12 @@ from topology.TopoLoador import TopoLoador
 # pretrain on torchvision
 
 def main():
-    parser = argparse.ArgumentParser(description='Decentralized image classification.')
+    parser = argparse.ArgumentParser(description='An implementation of PartPSP.')
 
     # basic config
-    parser.add_argument('--model', type=str, default='MLP', help='model name, options: [MLP, Resnet18, Resnet34]')
-    parser.add_argument('--shared_layers', type=int, default=1, help='number of shared layers, options: [1, 2, 3(full)]')
-    parser.add_argument('--pretrained', action='store_false', help='load pretrained model (only for extractor). Default is True.')
+    parser.add_argument('--model', type=str, default='MLP', help='model name, options: [MLP, Resnet18, ViT]')
+    parser.add_argument('--shared_layers', type=int, default=1, help='number of shared layers, options: [1, 2]')
+    parser.add_argument('--pretrained', action='store_false', help='load pretrained model (only for the shared parameters). Default is True.')
 
     # data loader
     parser.add_argument('--data', type=str, default='mnist', help='dataset, options: [mnist, fmnist, cifar10]')
@@ -29,13 +29,13 @@ def main():
     parser.add_argument('--eva_store_path', type=str, default='./evaluation', help='evaluation store root path')
 
     # optimization
-    parser.add_argument('--epochs', type=int, default=12, help='train epochs')
-    parser.add_argument('--batch_size', type=int, default=100, help='batch size of train input data')
-    parser.add_argument('--ext_lr', type=float, default=0.1, help='optimizer learning rate for extractor')
-    parser.add_argument('--cla_lr', type=float, default=0.1, help='optimizer learning rate for classifier')
+    parser.add_argument('--epochs', type=int, default=12, help='training epochs')
+    parser.add_argument('--batch_size', type=int, default=100, help='batch size of the train input data for each node')
+    parser.add_argument('--ext_lr', type=float, default=0.1, help='optimizer learning rate for the extractor (shared)')
+    parser.add_argument('--cla_lr', type=float, default=0.1, help='optimizer learning rate for the classifier (local)')
     parser.add_argument('--noi_lr', type=float, default=0.001, help='noise rate')
-    parser.add_argument('--ext_lr_decay', action='store_false', help='Set to enable learning rate decay for extractor. Default is True.')
-    parser.add_argument('--cla_lr_decay', action='store_false', help='Set to enable learning rate decay for classifier. Default is True.')
+    parser.add_argument('--ext_lr_decay', action='store_false', help='Set to enable learning rate decay for the extractor. Default is True.')
+    parser.add_argument('--cla_lr_decay', action='store_false', help='Set to enable learning rate decay for the classifier. Default is True.')
     parser.add_argument('--noi_lr_decay', action='store_false', help='Set to enable noise rate decay. Default is True.')
     parser.add_argument('--ext_loc_ite', type=int, default=1, help='local optimization round for extractor')
     parser.add_argument('--cla_loc_ite', type=int, default=1, help='local optimization round for classifier')
@@ -43,8 +43,8 @@ def main():
     # dp noise
     parser.add_argument('--dp_noise', action='store_true', help='add noise in consensus. Default is False.')
     parser.add_argument('--budget', type=float, default=5.0, help='privacy budget hyperparameter')
-    parser.add_argument('--C', type=float, default=0.5, help='hyperparameter C for sensitivity estimator')
-    parser.add_argument('--lamb', type=float, default=0.3, help='hyperparameter lambda for sensitivity estimator')
+    parser.add_argument('--C', type=float, default=0.5, help='hyperparameter C for the sensitivity estimator')
+    parser.add_argument('--lamb', type=float, default=0.3, help='hyperparameter lambda for the sensitivity estimator')
     parser.add_argument('--grad_clip', action='store_true', help='gradient clipping trigger. Default is False.')
     parser.add_argument('--clip_thre', type=float, default=1.0, help='gradient clipping threshold')
 
@@ -56,7 +56,7 @@ def main():
         default='tcp://127.0.0.1:',
         help='URL specifying how to initialize the package.')
     parser.add_argument('--port', default=14562, type=int, help='port')
-    parser.add_argument('-s', '--world-size', type=int, default=10, help='network Scale')
+    parser.add_argument('-s', '--world-size', type=int, default=10, help='network scale')
     parser.add_argument('--backend', type=str, default='gloo', help='Name of the backend to use.')
 
     # decentralized protocol settings
